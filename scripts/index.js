440e340e8db3432bc5ff0350dcea5073e63bad84
Ryan Farhoud <Ryanfarhoud@gmail.com>
2025-05-10 10:11:51 +0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "The Everest",
+    link: "file:///Users/ryanfarhoud/Desktop/se_project_aroundtheus/images/everest.jpg",
+  },
+  {
+    name: "Altai Republic",
+    link: "file:///Users/ryanfarhoud/Desktop/se_project_aroundtheus/images/altai_republic.jpg",
+  },
+  {
+    name: "The Amazon",
+    link: "file:///Users/ryanfarhoud/Desktop/se_project_aroundtheus/images/amazon.jpg",
+  },
+  {
+    name: "Fuji Mountain",
+    link: "file:///Users/ryanfarhoud/Desktop/se_project_aroundtheus/images/fuji_mountain.jpg",
+  },
+  {
+    name: "Easter Island Heads",
+    link: "file:///Users/ryanfarhoud/Desktop/se_project_aroundtheus/images/moai.jpg",
+  },
+  {
+    name: "The Pyramids Of Giza",
+    link: "file:///Users/ryanfarhoud/Desktop/se_project_aroundtheus/images/pyramids_of_giza.jpg",
+  },
+];
+
+console.log(initialCards);
