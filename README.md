85d996c66b715a19172c1d992e5ba101504fc962
ryanfarhoud <ryanfarhoud@gmail.com>
2026-04-02 15:04:18 +0300

    Final submission cleanup

---
+++ README.md
@@ -0,0 +1,74 @@
+# Project 3: Around The U.S.
+
+**Intro**
+
+_This is the third project in the full stack development program in TripleTen school. It was created using responsive design that can be viewed in phones, tablets and computers. It is build using HTML and CSS, Based on the design from figma._
+
+**Figma**
+
+- [Link to the project on Figma](https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1)
+
+**LINK**
+
+- [Link to the project](https://ryan-hitech.github.io/se_project_aroundtheus/)
+
+## Project Features
+
+- semantic HTML5
+
+* Flexbox
+* Grid
+* Responsive Design
+* Positioning
+* Flat BEM file structure
+
+- Media queries
+- CSS transition.
+
+## Description
+
+<<<<<<< HEAD
+[project video](https://drive.google.com/file/d/1qCmRMrNHV3ydibLGE_mJHiJlQVwx1SKH/view?usp=sharing)
+=======
+
+- Instructional video :
+
+<video controls src="prject3-video.mov" title="project3"></video>
+
+> > > > > > > a454655 (add readme)
+
+- For code i used the visual studio code or VS Code.
+
+* The project was build using BEM Methodology which stands for block , element , modifier.
+
+- The styling in css was done using the bem class naming method , and block were : card.css , footer.css, header.css, page.css, profile.css and they were all imported to index.css.
+  and then they were links to our index.html file.
+
+![import-css](images/import-project3.png)
+
+- AS it is shown on the screen shot , i put normilize.css and the top of the index.css and after it the fonts.css because in css browser reads it from the bottom up and it can be problematic to put normilize.css in the bottom .
+
+* For this project i used a responsive design that can be viewed in different screens like phone , laptops, tablets thank to media queries.
+  for this project i started in 1280 px and then adjusted the layout to the size of tablet using media querie 770px and then to the size of a phone using another media querie 580px.
+  in order to prevent elements from touching the sides of the screen in small computer i used the fourth and last media querie which is 905px.
+
+* Here are some images of the layouts in different sizes:
+
+1. Computer 1280px
+   ![pc-1280px](images/project3-1280px.png)
+
+2. Computer 905px
+   ![pc-905px](images/project3-905px.png)
+
+3. Tablets 770px
+   ![tablets-770px](images/project3-770px.png)
+
+4. Cellphones 580px
+   ![cellphones-580px](images/project3-580px.png)
+
+- For the layout i used Flexbox to position elements in a one directional axis .
+  and i used Grid for two dimensions like in the profile.css and card.css .
+
+**Future Plan**
+
+_Soon the ability to change profile picture and card images will be available_
---
+++ scripts/index.js
@@ -0,0 +1,76 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileCloseButton = document.querySelector(".modal__close-button");
+const profileTitle = document.querySelector("#profile-title");
+const profileDescription = document.querySelector("#profile-description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input",
+);
+const profileForm = document.forms["modal-form"];
+const cardListEl = document.querySelector(".card__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+  cardTitleEl.textContent = data.name;
+  return cardElement;
+}
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+
+  profileEditModal.classList.add("modal_opened");
+});
+profileCloseButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
+  cardListEl.prepend(cardElement);
+});
